dc6189be75cad55a8dc10a4ec2284155a80b52a7
dvub <dvubdevs@gmail.com>
2024-02-06 21:57:25 -0800

    Document clap

--- cli/src/args.rs
+++ cli/src/args.rs
@@ -3,22 +3,29 @@ use clap::{Parser, Subcommand};
 // TODO:
 // adequately document this shit lol
 
+/// CLI for Remind-me, a blazingly fast
+/// desktop reminder app written in Rust.
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 pub struct Args {
+    /// The type of subcommand to run
     #[command(subcommand)]
     pub command: Commands,
 }
 #[derive(Subcommand)]
 pub enum Commands {
+    /// Commands for managing the remind-me daemon.
+    /// Get info on daemon status, start, and stop daemon.
     Control {
         #[command(subcommand)]
         action: ControlCommands,
     },
+    /// Manage reminders through the CLI. Create, edit, or delete reminders. Alternatively, edit the file manually.
     Reminders {
         #[command(subcommand)]
         action: RemindersCommands,
     },
+    /// Auth commands. Login, logout, etc.
     Auth {
         #[command(subcommand)]
         action: AuthCommands,
@@ -40,7 +47,10 @@ pub enum RemindersCommands {
 
 #[derive(Subcommand)]
 pub enum ControlCommands {
+    /// Prints if the daemon is running or not.
     IsRunning,
+    /// Starts the daemon if it's not already running. Multiple instances are not supported at this time.
     Start,
+    /// Stops the daemon if it is running.
     Stop,
 }
