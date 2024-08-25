7812598d999731a9fc2cf77d5bd2006aef2d2fd7
dvub <dvubdevs@gmail.com>
2024-08-24 17:27:16 -0700

    improved documentation/comments for the CLI

--- cli/src/args.rs
+++ cli/src/args.rs
@@ -1,7 +1,7 @@
 use clap::{Parser, Subcommand};
 
-// TODO:
-// adequately document this shit lol
+// half of this file is just documentation
+// but the CLI is very well-explained now, at least. hahah
 
 /// CLI for Remind-me, a blazingly fast
 /// desktop reminder app written in Rust.
@@ -14,13 +14,12 @@ pub struct Args {
 }
 #[derive(Subcommand)]
 pub enum Commands {
-    /// Commands for managing the remind-me daemon.
-    /// Get info on daemon status, start, and stop daemon.
+    /// Commands for managing the remind-me backend.
     Control {
         #[command(subcommand)]
         action: ControlCommands,
     },
-    /// Manage reminders through the CLI. Create, edit, or delete reminders. Alternatively, edit the file manually.
+    /// Manage reminders through the CLI. Create, edit, or delete reminders. (Alternatively, edit the file manually.)
     Reminders {
         #[command(subcommand)]
         action: RemindersCommands,
@@ -29,33 +28,54 @@ pub enum Commands {
 
 #[derive(Subcommand)]
 pub enum RemindersCommands {
+    /// Add a new reminder. All arguments except for `icon` and `trigger_limit` are required.
     Add {
+        /// The name of the new reminder. This will be the name you see in notifications, etc. - so make it human-friendly.
         #[arg(long)]
         name: String,
+        /// The reminder's description.
         #[arg(long)]
         description: String,
+        /// The frequency, *in seconds*, that this reminder will trigger. Make sure to calculate out hours/minutes to seconds!
         #[arg(long)]
         frequency: i32,
+        /// An optional string which could be an emoji, etc. to help represent the reminder.
+        /// In desktop notifications, this is prepended to the name in the notification title.
         #[arg(long)]
         icon: Option<String>,
+        /// Optionally, supply a limit for how many times this reminder will trigger. If nothing is supplied, the reminder will trigger indefinitely.
+        #[arg(long)]
+        trigger_limit: Option<i32>,
     },
+    /// Update a reminder. Only the name of the existing reminder is required; every other argument is optional.
     Update {
+        /// The name of the *existing* reminder to update.
         #[arg(long)]
         name: String,
+        /// Optionally, you may update the reminder's name.
         #[arg(long)]
         new_name: Option<String>,
+        /// The reminder's new description.
         #[arg(long)]
         description: Option<String>,
+        /// The reminder's new frequency (in seconds).
         #[arg(long)]
         frequency: Option<i32>,
+        /// The reminder's new icon.
         #[arg(long)]
         icon: Option<String>,
+        /// The new max number of times this reminder may trigger.
+        #[arg(long)]
+        trigger_limit: Option<i32>,
     },
+    /// Deletes a reminder, removing its entry from the TOML file specified by the `path` subcommand.
     Delete {
         #[arg(long)]
         name: String,
     },
+    /// Prints all reminders. A specific message will be displayed if no reminders were found. (i.e, if the TOML file specified by the `path` subcommand is empty)
     List,
+    /// Displays the path to the TOML file containing reminders.
     Path,
 }
 
